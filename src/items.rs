73c6d5d996906d9ae5c6d71fc4ea51cce838cdc5
professionalowo <martin.brandl1307@gmail.com>
2023-10-01 16:42:57 +0200

    extracted to module

---
+++ src/items.rs
@@ -0,0 +1,22 @@
+use std::{collections::HashMap, sync::Mutex};
+use crate::page::{Auction, Page};
+
+pub struct ItemsCache {
+    api_key: String,
+    pub map: Mutex<HashMap<String, Vec<Auction>>>,
+}
+
+impl ItemsCache {
+    pub async fn new(api_key: String) -> Self {
+        let auctions = Page::get_map(&api_key).await.ok().unwrap();
+        Self {
+            map: auctions.into(),
+            api_key,
+        }
+    }
+    pub async fn update(&self) -> () {
+        let new_map = Page::get_map(&self.api_key).await.ok().unwrap();
+        let mut state = self.map.lock().expect("could not lock map");
+        *state = new_map;
+    }
+}
